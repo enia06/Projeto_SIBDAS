dc357bebaa1061db6bf0b5ab896357d6b03c31a1
Énia Torres <1241327@isep.ipp.pt>
2026-05-20 22:25:02 +0100

    Implementação do carousel na homepage e ajustes visuais

--- assets/js/1241327.js
+++ assets/js/1241327.js
@@ -1,31 +1,3 @@
-const imagens = [
-    "../assets/img/welcome 1.png",
-    "../assets/img/welcome 2.png"
-];
 
-let imagemAtual = 0;
-
-function atualizarImagem() {
-    document.getElementById("slider-image").src = imagens [imagemAtual];
-}
-
-function imagemSeguinte() {
-    imagemAtual++;
-    if (imagemAtual >= imagens.length) {
-        imagemAtual = 0;
-    }
-
-    atualizarImagem();
-}
-
-function imagemAnterior() {
-    imagemAtual--;
-    if (imagemAtual < 0) {
-        imagemAtual = imagens.length -1;
-    }
-
-    atualizarImagem();
-}
-        
 
 
